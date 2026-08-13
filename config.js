551d230c3b01c44f5a9a681f70c4ab652bed70b4
devnnex <devnnex@users.noreply.github.com>
2026-08-13 14:47:41 -0500

    Update Apps Script web app URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Pega aquí la URL del Web App de Google Apps Script cuando esté desplegado.
-window.DRINKS_API_URL = "https://script.google.com/macros/s/AKfycbwXmEEMnVDd2Suzv_fu3lHFEeO1yKND9e-H9lqthFXbT-FvKKLGBGFe9ngx2sVYg5td/exec";
+window.DRINKS_API_URL = "https://script.google.com/macros/s/AKfycbzW0VP0HvUbFNdutrD9RhocGlCvmIasyYeknqKPlJgXJqa68a1GEE-KlmfXs6ITzHmr/exec";
